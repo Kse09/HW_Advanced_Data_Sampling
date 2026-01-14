b9a6700f57872288de1067d0ec8f7f592e8cec8b
Ксения Ерёмина <what.about.ksenia@gmail.com>
2026-01-14 20:23:06 +0300

    add select1

---
+++ Select1.sql
@@ -0,0 +1,21 @@
+SELECT track_name, duration
+FROM track 
+WHERE duration = (SELECT MAX(duration) FROM track);
+
+SELECT track_name, duration
+FROM track 
+WHERE duration >= 210
+ORDER BY duration;
+
+SELECT collection_name, collection_release
+FROM collection 
+WHERE collection_release BETWEEN 2018 AND 2020
+ORDER BY collection_release;
+
+SELECT name
+FROM artist 
+WHERE name NOT LIKE '% %';
+
+SELECT track_name 
+FROM track 
+WHERE lower(track_name) LIKE '%my%' OR LOWER(track_name) LIKE '%мой%';
